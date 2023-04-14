9dd06ab0bf70fe4bc3d03534747ad42d7f98922a
юра деворак <indacloset@ya.ru>
2023-04-14 19:47:24 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, jsonify
+import logging
+import resourses
+import random as r
+
+app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.INFO)
+
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+
+    logging.info(f'Request: {request.json!r}')
+
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info(f'Response:  {response!r}')
+
+    return jsonify(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        res['response']['text'] = 'Привет! Хочешь получить смешное фото кота?'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+
+    if req['request']['original_utterance'].lower() in [
+        'ладно',
+        'конечно',
+        'да',
+        'хорошо'
+    ]:
+        # Пользователь согласился, прощаемся.
+        res['response']['text'] = 'Держи!'
+        res['response']['card']['type'] = "BigImage"
+        res['response']['card']['image_id'] = r.choice(resourses.imgs)
+
+        if req['request']:
+            res['response']['text'] = 'Хотите ещё картинку?'
+
+    res['response']['text'] = \
+        f"Ну хорошо"
+    return
+
+
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    suggests.append({
+        "title": "Ладно",
+        "url": "https://market.yandex.ru/search?text=слон",
+        "hide": True
+    })
+
+    return suggests
+
+
+if __name__ == '__main__':
+    app.run()
